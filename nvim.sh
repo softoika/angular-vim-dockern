47c702b0a6ee20829a7c0cf083952ee818772deb
r-hanafusa <r_hanafusa@dreamarts.co.jp>
2020-10-12 21:54:58 +0900

    Support neovim

--- nvim.sh
+++ nvim.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-IMAGE=docker-vim
-docker build -t $IMAGE -f ./vim/Dockerfile .
+IMAGE=docker-nvim
+docker build -t $IMAGE -f ./neovim/Dockerfile .
 docker system prune -f
 docker run -v ${PWD}/tour-of-heroes:/root/tour-of-heroes \
   -v ${HOME}/Repositories/ngswitcher.vim:/root/ngswitcher.vim --rm -it $IMAGE bash
