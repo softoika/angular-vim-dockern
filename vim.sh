3bfeb1787ee2e8ecf663c36d2ff7bc0aed155f8c
r-hanafusa <r_hanafusa@dreamarts.co.jp>
2020-10-12 20:48:28 +0900

    Reference local ngswitcher.vim for development

--- vim.sh
+++ vim.sh
@@ -2,5 +2,6 @@
 IMAGE=docker-vim
 docker build -t $IMAGE .
 docker system prune -f
-docker run -v ${PWD}/tour-of-heroes:/root/tour-of-heroes --rm -it $IMAGE bash
+docker run -v ${PWD}/tour-of-heroes:/root/tour-of-heroes \
+  -v ${HOME}/Repositories/ngswitcher.vim:/root/ngswitcher.vim --rm -it $IMAGE bash
 
